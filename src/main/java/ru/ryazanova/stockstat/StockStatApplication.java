57f47974851695058b598f2da5502f6fe8b49c74
Ekaterina Ryazanova <Ekaterina_Ryazanova@epam.com>
2021-01-31 15:14:55 +0300

    Delete logger

--- src/main/java/ru/ryazanova/stockstat/StockStatApplication.java
+++ src/main/java/ru/ryazanova/stockstat/StockStatApplication.java
@@ -4,21 +4,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.ryazanova.stockstat.model.Company;
+import ru.ryazanova.stockstat.service.CompanyService;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 @SpringBootApplication
 public class StockStatApplication implements CommandLineRunner {
-    private static Logger LOG = (Logger) LoggerFactory.getLogger(StockStatApplication.class);
-
     public static void main(String[] args) {
         SpringApplication.run(StockStatApplication.class, args);
-        LOG.info("STARTING THE APPLICATION");
     }
-
 
     @Override
     public void run(String... args) throws Exception {
         System.out.println("StockStat app is working ...");
+
+        new CompanyService().findAll().forEach(x -> System.out.println(x));
     }
 }
